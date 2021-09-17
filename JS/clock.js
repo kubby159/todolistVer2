53deed815acd437185c27f3e1a0ba4acde284359
kubby159 <kubby159@naver.com>
2021-09-17 23:33:49 +0900

    clock

---
+++ JS/clock.js
@@ -0,0 +1,19 @@
+
+
+const clock = document.getElementById('clock');
+
+console.log(clock);
+
+function getClock () {
+
+
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,0);
+    const minutes = String(date.getMinutes()).padStart(2,0);
+
+    clock.innerHTML = `${hours} : ${minutes}`;
+
+}
+
+getClock();
+setInterval(getClock, 1000);
