bd80eb6c3b0c4f2021aecc77168253b5125d30df
kubby159 <kubby159@naver.com>
2021-09-18 00:02:30 +0900

    bg-color

--- JS/clock.js
+++ JS/clock.js
@@ -10,6 +10,7 @@ function getClock () {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,0);
     const minutes = String(date.getMinutes()).padStart(2,0);
+   
 
     clock.innerHTML = `${hours} : ${minutes}`;
 
