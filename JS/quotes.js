a585ee5da32174c362be722fd69b5500a0696a33
unknown <kubby159@naver.com>
2022-01-21 22:35:27 +0900

    [Todolist] Updated

--- JS/quotes.js
+++ JS/quotes.js
@@ -1,28 +1,40 @@
+const quotes = document.querySelector(".quotes-box");
+const quotesAuthor = quotes.querySelector(".author");
+const quotesMain = quotes.querySelector(".quotes");
 
+// backGroundImage
+const backImgBody = document.querySelector("body");
 
-const quotes = document.querySelector('.quotes-box');
-const quotesAuthor = quotes.querySelector('.author');
-const quotesMain = quotes.querySelector('.quotes');
+// backGroundImageCollection
 
-console.log(quotesAuthor);
-console.log(quotesMain);
-
+const backGroundImage = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg"];
 
 const quotesCollection = [
-    {text: `“Be yourself, everyone else is already taken.”`, author: "Oscar Wilde"},
-    {text: `“So many books, so little time.”`, author: "Frank Zappa"},
-    {text: `Marcus Tullius Cicero
-    “A room without books is like a body without a soul.”`, author: "Marcus Tullius Cicero"},
-    {text: `“You only live once, but if you do it right, once is enough.”`, author: "Mae West"}
-]
+  {
+    text: `“Be yourself, everyone else is already taken.”`,
+    author: "Oscar Wilde",
+  },
+  { text: `“So many books, so little time.”`, author: "Frank Zappa" },
+  {
+    text: `Marcus Tullius Cicero
+    “A room without books is like a body without a soul.”`,
+    author: "Marcus Tullius Cicero",
+  },
+  {
+    text: `“You only live once, but if you do it right, once is enough.”`,
+    author: "Mae West",
+  },
+];
 
-const randomSelector = quotesCollection[Math.floor(Math.random()*quotesCollection.length)];
+const randomSelector =
+  quotesCollection[Math.floor(Math.random() * quotesCollection.length)];
 
 function addQuotes() {
-
-   quotesMain.innerText = randomSelector.text;
-   quotesAuthor.innerText = randomSelector.author;         
-    
+  quotesMain.innerText = randomSelector.text;
+  quotesAuthor.innerText = randomSelector.author;
+  backImgBody.style.backgroundImage = `url('./image/${
+    backGroundImage[Math.floor(Math.random() * backGroundImage.length)]
+  }')`;
 }
 
-addQuotes()
+addQuotes();
--- JS/todo.js
+++ JS/todo.js
@@ -1,75 +1,68 @@
-const todoForm = document.querySelector('.todo-form');
-const todoInput = document.querySelector('.todo-input');
-const todoList = document.querySelector('.todo-list');
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
+const todoList = document.querySelector(".todo-list");
 
-const TODOS_KEY = 'toDos'
+const TODOS_KEY = "toDos";
 
 let toDos = [];
 
-function  saveTodos() {
-    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
-
 
 function deleteList(event) {
-    const deleteEvent = event.target.parentElement;
-    deleteEvent.remove();
-   
-    toDos = toDos.filter(toDo => toDo.id !== parseInt(deleteEvent.id));
-    
-   
-    saveTodos();
+  const deleteEvent = event.target.parentElement;
+  deleteEvent.remove();
+
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteEvent.id));
+
+  saveTodos();
 }
-
-
 
 function paintToDo(newTodo) {
+  const list = document.createElement("li");
+  const btn = document.createElement("button");
+  const span = document.createElement("span");
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.className = "checkbox";
+  list.appendChild(span);
+  list.appendChild(btn);
+  list.prepend(input);
 
-    const list = document.createElement('li');
-    const btn = document.createElement('button');
-    const span = document.createElement('span');
-
-
-    list.appendChild(span);
-    list.appendChild(btn);
-    span.innerText = newTodo.text;
-    list.id = newTodo.id;
-    btn.innerText = "❌";
-    btn.addEventListener('click',deleteList);
-    todoList.appendChild(list);
-    
+  span.innerText = `${newTodo.text}`;
+  list.id = newTodo.id;
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteList);
+  todoList.appendChild(list);
+  //completed
+  input.addEventListener("click", (e) => {
+    span.classList.toggle("completed");
+  });
 }
-
-
 
 function handleToDoSubmit(event) {
-    
-    event.preventDefault();
-    const newTodo = todoInput.value;
-   
-    todoInput.value = "";
-    const newToDoObj = {
-        text: newTodo,
-        id : Date.now()
-    }
-    toDos.push(newToDoObj);
-    paintToDo(newToDoObj);
-    
-    saveTodos();
+  event.preventDefault();
+  const newTodo = todoInput.value;
+
+  todoInput.value = "";
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+
+  saveTodos();
 }
 
-
-
-
-todoForm.addEventListener('submit',handleToDoSubmit);
+todoForm.addEventListener("submit", handleToDoSubmit);
 
 const getValue = localStorage.getItem(TODOS_KEY);
 console.log(getValue);
 
-
-if(getValue !== null) {
-
-    const parsedToDos = JSON.parse(getValue);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
-;}
+if (getValue !== null) {
+  const parsedToDos = JSON.parse(getValue);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
