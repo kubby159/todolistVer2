578288c5bc3d4a427fb2fc8c5842eb09cd60f866
unknown <kubby159@naver.com>
2022-01-21 23:46:47 +0900

    [JS] set weather

---
+++ JS/weather.js
@@ -0,0 +1,36 @@
+const weatherBox = document.querySelector(".header .weather");
+
+let weatherIcon = {
+  "01": "far fa-sun",
+  "02": "far fa-cloud-sun",
+  "03": "far fa-cloud",
+  "04": "far fa-cloud-meatball",
+  "09": "far fa-cloud-sun-rain",
+  10: "far fa-cloud-showers-heavy",
+  11: "far fa-poo-storm",
+  13: "far fa-snowflake",
+  50: "far fa-smog",
+};
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=Incheon&appid=e4de3cfc2c15755cff069e3c393e4310`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    weatherBox.innerHTML = `<img class ='loading' src= './image/Rolling-1s-200px.gif' />`;
+
+    let icon = data.weather[0].icon.substr(0, 2);
+    let temp = String(Math.floor(data.main.temp)).substr(0, 2) + "º";
+    const city = data.name;
+    setTimeout(() => {
+      weatherBox.innerHTML = `
+    <div class = 'show-weather'>
+    <span class = 'show-icon'><i class= '${weatherIcon[icon]}'></i></span>
+    <span class= 'show-temp'>${temp}</span>
+    </div>
+
+    <span class= 'show-city'>${city}</span>
+
+    `;
+    }, 2000);
+  });
