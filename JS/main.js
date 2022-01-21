0837c4a814688e22c08400e5b866749c4eb3decb
unknown <kubby159@naver.com>
2022-01-22 00:03:59 +0900

    [JS] revised todolist

--- JS/main.js
+++ JS/main.js
@@ -1,41 +1,33 @@
-const loginInput = document.querySelector('#login-form input');
-const loginForm = document.querySelector('#login-form');
-const loginBox = document.querySelector('.login__box');
-const greetingBox = document.querySelector('.greeting-box h1');
-const greetingBoxParent = document.querySelector('.greeting-box');
+const loginInput = document.querySelector(".login-form input");
+const loginForm = document.querySelector(".login-form");
+const loginBox = document.querySelector(".login__box");
+const greetingBox = document.querySelector(".greeting-box h1");
+const greetingBoxParent = document.querySelector(".greeting-box");
+const todolistBox = document.querySelector(".todolist-box");
 
-console.log(greetingBox);
-
-const USERNAME_KEY = 'username';
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    
-    event.preventDefault();
-    const userName = loginInput.value;
-    loginInput.value = "";
-    loginBox.classList.add('hidden');
-    localStorage.setItem("USERNAME_KEY",userName);
-    paintGreeting(userName);
+  event.preventDefault();
+  const userName = loginInput.value;
+  loginInput.value = "";
+  loginBox.classList.add("hidden");
+  localStorage.setItem("USERNAME_KEY", userName);
+  paintGreeting(userName);
 }
-
 
 function paintGreeting(userName) {
- 
-    
-    greetingBox.innerText = `Nice to Meet you, ${userName}`;
-    greetingBoxParent.classList.remove('hidden');
+  greetingBox.innerText = `Nice to Meet you, ${userName}`;
+  greetingBoxParent.classList.remove("hidden");
 }
 
+const savedUserName = localStorage.getItem("USERNAME_KEY");
 
-const savedUserName = localStorage.getItem('USERNAME_KEY');
-
-
-if(savedUserName === null) {
-
-    loginBox.classList.remove('hidden');
-    loginForm.addEventListener('submit',onLoginSubmit);
+if (savedUserName === null) {
+  loginBox.classList.remove("hidden");
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    loginBox.classList.add('hidden');
-    paintGreeting(savedUserName);
-}
+  loginBox.classList.add("hidden");
 
+  paintGreeting(savedUserName);
+}
